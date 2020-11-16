1101285c835ff7c3ac458e5632d8fff2d406af04
popper2710 <hige1332@yahoo.co.jp>
2020-11-16 23:58:17 +0900

    [add] add model files

---
+++ src/model/contest_result.py
@@ -0,0 +1,9 @@
+import datetime
+
+
+class ContestResult:
+    contestName = ""
+    performance = -1
+    new_rating = -1
+    end_time = datetime.datetime.now()
+
---
+++ src/model/user.py
@@ -0,0 +1,10 @@
+import datetime
+
+
+class User:
+    username = ""
+    current_rating = -1
+    contest_results = []
+    last_updated = datetime.datetime.now()
+
+
