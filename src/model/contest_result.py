346daf2df865451475690a08652aebd44469e054
popper2710 <hige1332@yahoo.co.jp>
2020-12-31 23:45:34 +0900

    [update] add "ranking" parameter to ContestResult

--- src/model/contest_result.py
+++ src/model/contest_result.py
@@ -7,6 +7,7 @@ class ContestResult:
     contestName = ""
     username = ""
     new_rating = -1
+    ranking = -1
     contest_start_time = -1
     elapsed_time = -1
 
@@ -15,6 +16,7 @@ class ContestResult:
             "contestName": self.contestName,
             "username": self.username,
             "new_rating": self.new_rating,
+            "ranking": self.ranking,
             "contest_start_time": self.contest_start_time,
             "elapsed_time": self.elapsed_time
         }
@@ -25,6 +27,7 @@ class ContestResult:
         result.contestName = result_dict["contestName"]
         result.username = result_dict["username"]
         result.new_rating = result_dict["new_rating"]
+        result.ranking = result_dict["ranking"]
         result.contest_start_time = result_dict["contest_start_time"]
         result.elapsed_time = result_dict["elapsed_time"]
         return result
