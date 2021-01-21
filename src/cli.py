cc1b94450ddab10b40e39e31a8a4adcf8a7332f0
popper2710 <hige1332@yahoo.co.jp>
2021-01-21 23:48:47 +0900

    [update] implement "-p" and "-u" arguments to "login" command

--- src/cli.py
+++ src/cli.py
@@ -7,6 +7,10 @@ LOGIN_COUNT = 3
 
 def login(args):
     manager = Manager()
+    if args.username and args.password:
+        manager.fetcher.login(args.username, args.password)
+        if manager.fetcher.is_login:
+            print("Username or password is incorrect")
     for _ in range(LOGIN_COUNT):
         if manager.fetcher.is_login:
             break
@@ -33,6 +37,8 @@ def main():
     parser = argparse.ArgumentParser(description="Find your rival in Atcoder")
     subparsers = parser.add_subparsers()
     parser_login = subparsers.add_parser("login", help="login to atcoder")
+    parser_login.add_argument("-u", "--username", help="Atcoder username")
+    parser_login.add_argument("-p", "--password", help="Atcoder password")
     parser_login.set_defaults(handler=login)
 
     parser_update = subparsers.add_parser("update", help="find and update rivals")
